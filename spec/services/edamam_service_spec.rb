baf509aa1029dfa10726d82e76ab69047dc0eb6c
Blaine Glasgow <glasgowb@outlook.com>
2023-08-06 22:49:03 -0700

    update edamam service

--- spec/services/edamam_service_spec.rb
+++ spec/services/edamam_service_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe EdamamService do
     @thai_recipe = EdamamService.new.search("thailand")
     @thai_recipe[:hits].each do |recipe|
       expect(recipe[:recipe]).to be_a(Hash)
+      expect(recipe[:recipe][:label]).to be_a(String)
       expect(recipe[:recipe][:ingredientLines]).to be_a(Array)
       expect(recipe[:recipe][:calories]).to be_a(Float)
       expect(recipe[:recipe][:mealType]).to be_a(Array)
