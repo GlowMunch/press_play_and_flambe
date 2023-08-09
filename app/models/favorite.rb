4fa789ee06cfbdb0b6ca9ceb62ad6e7f6f6f2186
Blaine Glasgow <glasgowb@outlook.com>
2023-08-09 06:08:40 -0700

    Create User Favorites

---
+++ app/models/favorite.rb
@@ -0,0 +1,6 @@
+class Favorite < ApplicationRecord
+  belongs_to :user
+  validates :country, presence: true
+  validates :recipe_link, presence: true
+  validates :recipe_title, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :favorites
   before_create :generate_api_key
   has_secure_password
 
