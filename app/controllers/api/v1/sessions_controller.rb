6fa0c60f4e3f2c51811725a264a20c5b093895f3
Blaine Glasgow <glasgowb@outlook.com>
2023-08-09 05:43:26 -0700

    Removed cookies and session from session controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,13 +1,17 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: user_params[:email])
     if user && user.authenticate(params[:password])
-      coockie[:api_key] = user.api_key
-      session[:user_id] = user.id
       render json: user, status: :ok
     else
       render json: { error: "Invalid email or password" }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:session).permit(:email, :password)
   end
 end
 
